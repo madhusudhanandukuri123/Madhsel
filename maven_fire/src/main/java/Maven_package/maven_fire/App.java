427e859801a8cab36e22e1cb39e81980c6f8e459
madhusudhanandukuri123 <madhusudhanandukur@gmail.com>
2022-10-18 22:40:37 +0530

    done

---
+++ maven_fire/src/main/java/Maven_package/maven_fire/App.java
@@ -0,0 +1,13 @@
+package Maven_package.maven_fire;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
